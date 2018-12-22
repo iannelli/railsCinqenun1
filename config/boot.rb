de0bdacd6cad7968a29de50e1a790c5c4203ee40
iannelli <contact@apligiciel.fr>
2018-12-22 18:05:10 +0100

    Update boot.rb

--- config/boot.rb
+++ config/boot.rb
@@ -1,7 +1,4 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-require 'bootsnap' # Speed up boot time by caching expensive operations.
-Bootsnap.setup(
-  cache_dir: 'tmp/readonly'
-)
+require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
