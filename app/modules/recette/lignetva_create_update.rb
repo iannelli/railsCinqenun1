d013ead8420d0389789791deb4e586202a93ab23
iannelli <contact@apligiciel.fr>
2020-01-18 16:15:36 +0100

    Maj du 18/01/2020

--- app/modules/recette/lignetva_create_update.rb
+++ app/modules/recette/lignetva_create_update.rb
@@ -1,79 +1,44 @@
 module LignetvaCreateUpdate
-    # Création/Maj éventuelle des lignes de la Déclaration de TVA
+    # Création (y compris écriture négative) des lignes de la Déclaration de TVA
     # Si Franchise TVA[Perte exonération] ou Régime RSI
-    def lignetva_create_update_trait
-        if @recette.montantHt.to_i < 0 && @recette.montantTva.to_i != 0
-            @recetteInitiale = Recette.where("parametreId = ? AND facRef = ?", @paramun.id, @recette.facRef).first
-            if @recetteInitiale.nil?
-                @erreur = Erreur.new
-                @erreur.dateHeure = @current_time.strftime "%d/%m/%Y %H:%M:%S"
-                @erreur.appli = "rails - RecettesController - create - lignetva_create_update_trait"
-                @erreur.origine = "erreur Find Recette Initiale à annuler"
-                @erreur.numLigne = '79'
-                #@erreur.message = e.message
-                @erreur.parametreId = params[:parametre][:id].to_s
-                @erreur.save
-                @CreateOK = 1
+    def lignetva_create_update_trait       
+        @ligneArrayRecette = []
+        @ligneArrayRecette = @recette.lignesTva.split("|")
+        i = 0
+        while i < @ligneArrayRecette.length
+            decla = @ligneArrayRecette[i]
+            i += 1
+            periode = @ligneArrayRecette[i]
+            i += 1
+            codeLigne = @ligneArrayRecette[i]
+            i += 1
+            baseHt = @ligneArrayRecette[i].to_i
+            i += 1
+            tva = @ligneArrayRecette[i].to_i
+            i += 1
+            @lignetva = Lignetva.where("parametreId = ? AND tvaDecla = ? AND tvaPeriode = ? AND tvaCodeLigne = ?", @paramun.id, decla, periode, codeLigne).first
+            if @lignetva.nil?
+                @lignetva = Lignetva.new
+                @lignetva.tvaDecla = decla
+                @lignetva.tvaPeriode = periode
+                @lignetva.tvaCodeLigne = codeLigne
+                @lignetva.tvaBase = baseHt.to_s
+                @lignetva.tvaMontant = tva.to_s
+                @lignetva.listeRecetteId = @recette.id
+                @lignetva.parametreId = @paramun.id
             else
-                if @paramun.lignetvas.length != 0
-                    @paramun.lignetvas.each do |lignetva|
-                        @arrayRecetteId = []
-                        if lignetva.listeRecetteId.blank? == false
-                            @arrayRecetteId = lignetva.listeRecetteId.split(",")
-                        end
-                        if @arrayRecetteId.include?(@recetteInitiale.id.to_s)
-                            mont = lignetva.tvaBase.to_i + @recette.montantHt.to_i
-                            lignetva.tvaBase = mont.to_s
-                            mont = lignetva.tvaMontant.to_i + @recette.montantTva.to_i
-                            lignetva.tvaMontant = mont.to_s
-                            @arrayRecetteId << @recette.id.to_s
-                            @arrayRecetteId.uniq! #Unification des id de même valeur en un seul id
-                            lignetva.listeRecetteId = @arrayRecetteId.join(',')
-                            lignetva.save
-                        end
-                    end
+                calTemp = @lignetva.tvaBase.to_i + baseHt
+                @lignetva.tvaBase = calTemp.to_s
+                calTemp = @lignetva.tvaMontant.to_i + tva
+                @lignetva.tvaMontant = calTemp.to_s
+                if @lignetva.listeRecetteId.blank? == false
+                    @arrayRecetteId = @lignetva.listeRecetteId.split(',')
                 end
+                @arrayRecetteId << @recette.id.to_s
+                @arrayRecetteId.uniq! #Unification des id de même valeur en un seul id
+                @lignetva.listeRecetteId = @arrayRecetteId.join(',')
             end
-        end
-        if @recette.montantHt.to_i > 0
-            @ligneArrayRecette = []
-            @ligneArrayRecette = @recette.lignesTva.split("|")
-            i = 0
-            while i < @ligneArrayRecette.length
-                decla = @ligneArrayRecette[i]
-                i += 1
-                periode = @ligneArrayRecette[i]
-                i += 1
-                codeLigne = @ligneArrayRecette[i]
-                i += 1
-                baseHt = @ligneArrayRecette[i].to_i
-                i += 1
-                tva = @ligneArrayRecette[i].to_i
-                i += 1
-                @lignetva = Lignetva.where("parametreId = ? AND tvaDecla = ? AND tvaPeriode = ? AND tvaCodeLigne = ?", @paramun.id, decla, periode, codeLigne).first
-                if @lignetva.nil?
-                    @lignetva = Lignetva.new
-                    @lignetva.tvaDecla = decla
-                    @lignetva.tvaPeriode = periode
-                    @lignetva.tvaCodeLigne = codeLigne
-                    @lignetva.tvaBase = baseHt.to_s
-                    @lignetva.tvaMontant = tva.to_s
-                    @lignetva.listeRecetteId = @recette.id
-                    @lignetva.parametreId = @paramun.id
-                else
-                    calTemp = @lignetva.tvaBase.to_i + baseHt
-                    @lignetva.tvaBase = calTemp.to_s
-                    calTemp = @lignetva.tvaMontant.to_i + tva
-                    @lignetva.tvaMontant = calTemp.to_s
-                    if @lignetva.listeRecetteId.blank? == false
-                        @arrayRecetteId = @lignetva.listeRecetteId.split(',')
-                    end
-                    @arrayRecetteId << @recette.id.to_s
-                    @arrayRecetteId.uniq! #Unification des id de même valeur en un seul id
-                    @lignetva.listeRecetteId = @arrayRecetteId.join(',')
-                end
-                @lignetva.save
-            end
+            @lignetva.save
         end
     end
 end
